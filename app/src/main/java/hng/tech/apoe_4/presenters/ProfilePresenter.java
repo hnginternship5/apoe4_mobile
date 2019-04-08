e39327f851e2eb32e73c1d644a76f086aab594fd
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-08 20:38:59 +0100

    Begin presenter and view for profile, pending api call

---
+++ app/src/main/java/hng/tech/apoe_4/presenters/ProfilePresenter.java
@@ -0,0 +1,16 @@
+package hng.tech.apoe_4.presenters;
+
+import hng.tech.apoe_4.views.ProfileView;
+
+public class ProfilePresenter {
+
+    ProfileView profileView;
+
+    public ProfilePresenter(ProfileView profileView) {
+        this.profileView = profileView;
+    }
+
+    public void fetchData(){
+
+    }
+}
---
+++ app/src/main/java/hng/tech/apoe_4/views/ProfileView.java
@@ -0,0 +1,8 @@
+package hng.tech.apoe_4.views;
+
+public interface ProfileView {
+    void beginDataFetch();
+    void onSuccess();
+    void onError();
+    void finiishProcess();
+}
