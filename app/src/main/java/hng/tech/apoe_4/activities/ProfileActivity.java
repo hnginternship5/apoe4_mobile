f5a77dec0c67f6c470f2430fd1e5e7e29ae297f0
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-08 19:49:10 +0100

    Begin ProfileActivity

---
+++ app/src/main/java/hng/tech/apoe_4/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package hng.tech.apoe_4.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hng.tech.apoe_4.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
