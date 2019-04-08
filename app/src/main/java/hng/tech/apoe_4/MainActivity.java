94c6fdea5f6817a5b1d165df534cc87f9b6d428b
Kufo2018 <ibukunolwalewa@gmail.com>
2019-04-08 19:56:11 +0100

    Dummy login screen activity

--- app/src/main/java/hng/tech/apoe_4/MainActivity.java
+++ app/src/main/java/hng/tech/apoe_4/MainActivity.java
@@ -1,13 +1,30 @@
 package hng.tech.apoe_4;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring views
+    EditText userName, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            // Initializing views
+            userName = findViewById(R.id.editText);
+            password = findViewById(R.id.editText2);
+            login = findViewById(R.id.button);
+
+        }
     }
-}
