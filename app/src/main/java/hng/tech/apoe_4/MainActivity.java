c5d976295df798e4b2d03e611a1b9276c7b20cea
Kulloveth <kulloveth@gmail.com>
2019-04-09 08:28:10 +0200

    added splash screen

--- app/src/main/java/hng/tech/apoe_4/MainActivity.java
+++ app/src/main/java/hng/tech/apoe_4/MainActivity.java
@@ -1,13 +1,24 @@
 package hng.tech.apoe_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       button= findViewById(R.id.gettingstarted);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,splashScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/hng/tech/apoe_4/slideAdapter.java
@@ -0,0 +1,66 @@
+package hng.tech.apoe_4;
+
+import android.content.Context;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class slideAdapter extends PagerAdapter{
+
+    Context context;
+    LayoutInflater layoutInflater;
+    public slideAdapter(Context context){
+this.context=context;
+    }
+public int [] slidde_immage ={
+       R.drawable.privacy,
+        R.drawable.safety,
+        R.drawable.care
+};
+
+
+public String[]slide_headings={
+"Your privacy and Time is",
+        "Your Privacy and Time is",
+        "Individual care for every"
+    };
+
+    public String[]slide_subheadings={
+            "            Safe With US",
+            "            Safe With US",
+            "               Patients"
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject( View view,  Object o) {
+        return view==( RelativeLayout)o;
+    }
+    public Object instantiateItem(ViewGroup container, int position){
+        layoutInflater=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.slidelayout,container,false);
+
+ImageView slideImageView=view.findViewById(R.id.imageView);
+TextView slideheadings=(TextView)view.findViewById(R.id.editText);
+        TextView slidesubheadings=view.findViewById(R.id.subheading);
+
+slideImageView.setImageResource(slidde_immage[position]);
+slideheadings.setText(slide_headings[position]);
+slidesubheadings.setText(slide_subheadings[position]);
+container.addView(view);
+        return view;
+    }
+    public void destroyItem(ViewGroup container, int position, Object object){
+container.removeView((RelativeLayout)object);
+    }
+}
