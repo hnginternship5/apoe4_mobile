ce7f7b4cf520cc1be3a3f95d8d57488d6f6f364a
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-09 07:49:33 +0100

    Update activity_register to androidx

--- app/src/main/java/hng/tech/apoe_4/RegisterActivity.java
+++ app/src/main/java/hng/tech/apoe_4/RegisterActivity.java
@@ -1,11 +1,12 @@
 package hng.tech.apoe_4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,5 +27,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
     }
 }
