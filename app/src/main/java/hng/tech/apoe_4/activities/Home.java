7944ef53f6b38da5f32a8939600cc8d49fb0e231
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-10 21:23:12 +0100

    Complete Bottom Navigation, Create Today Fragment

---
+++ app/src/main/java/hng/tech/apoe_4/activities/Home.java
@@ -0,0 +1,78 @@
+package hng.tech.apoe_4.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hng.tech.apoe_4.R;
+import hng.tech.apoe_4.fragments.TodayFragment;
+
+public class Home extends AppCompatActivity {
+
+
+    @BindView(R.id.navigationView)
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+        ButterKnife.bind(this);
+
+
+
+        BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
+            switch (item.getItemId()){
+                case R.id.navigation_results:{
+
+                    return true;
+                }
+
+                case R.id.navigation_today:{
+                    openFragment(TodayFragment.newInstance());
+                    return true;
+                }
+
+                case R.id.navigation_forum:{
+
+                    return true;
+                }
+
+                default:{
+
+                    return true;
+                }
+            }
+        };
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(listener);
+
+
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
