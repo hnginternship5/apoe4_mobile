15b720db0880765b7db50318bf711875b08cd449
Nnacheta Chimdike <yungdyke@gmail.com>
2019-04-11 23:24:08 +0100

    Location issue solved

--- app/src/main/java/hng/tech/apoe_4/fragments/TodayFragment.java
+++ app/src/main/java/hng/tech/apoe_4/fragments/TodayFragment.java
@@ -2,11 +2,13 @@ package hng.tech.apoe_4.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -22,6 +24,8 @@ import hng.tech.apoe_4.R;
 import hng.tech.apoe_4.retrofit.ApiInterface;
 import hng.tech.apoe_4.retrofit.responses.WeatherResponse;
 import hng.tech.apoe_4.utils.MainApplication;
+import hng.tech.apoe_4.utils.ProgressAnim;
+import im.delight.android.location.SimpleLocation;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -35,13 +39,42 @@ public class TodayFragment extends Fragment {
     @BindView(R.id.tempProgress)
     ProgressBar tempProgress;
 
+    @BindView(R.id.stepsProgress)
+    ProgressBar stepsProgress;
+
+    @BindView(R.id.sleepProgress)
+    ProgressBar sleepProgress;
+
     @BindView(R.id.temp)
     TextView tempText;
+
+    private float from = (float)10;
+    private float to;
+    private String temp;
+    double progress, lat, lng;
+    SimpleLocation location;
+    Context mContext = getActivity();
+    char degree = '\u00B0';
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_today, container, false);
+
+// construct a new instance of SimpleLocation
+        location = new SimpleLocation(getActivity());
+
+        // if we can't access the location yet
+        if (!location.hasLocationEnabled()) {
+            // ask the user to enable location access
+            Toast.makeText(getActivity(), "Enable Location Permission", Toast.LENGTH_LONG).show();
+            SimpleLocation.openSettings(getActivity());
+        }else {
+            lat  =location.getLatitude();
+            lng= location.getLongitude();
+        }
+
 
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
 
@@ -68,17 +101,22 @@ public class TodayFragment extends Fragment {
 
         ApiInterface apiInterface = mRetrofit.create(ApiInterface.class);
 
-        apiInterface.getWeather(132.0, 145.0).enqueue(new Callback<WeatherResponse>() {
+        apiInterface.getWeather(lat, lng).enqueue(new Callback<WeatherResponse>() {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
                 if (response.isSuccessful()) {
                     double temp = response.body().getMain().getTemp();
                     double tempMax = response.body().getMain().getTempMax();
+                    Log.d("TAG", "temp: " + temp);
+                    Log.d("TAG", "tempMax: " + tempMax);
 
-                    double progress = (temp / tempMax) * 100;
+                    progress = (temp / tempMax) * 100;
+                    Log.d("TAG", "progress: " + progress);
 
-                    tempProgress.setProgress((int) progress);
-                    tempText.setText(String.valueOf((int) temp) + "C");
+//                    tempProgress.setProgress((int) progress);
+                    setAnimation();
+
+                    tempText.setText(String.valueOf((int) temp) + degree +"C");
                 }
             }
 
@@ -87,7 +125,37 @@ public class TodayFragment extends Fragment {
 
             }
         });
+
+        getSleepTime();
+        getStepNumber();
+
+
         return view;
+    }
+
+    //this method helps with animating progress bar
+
+    private void setAnimation() {
+        to = (float)progress;
+        ProgressAnim anim = new ProgressAnim(tempProgress, from, to);
+        anim.setDuration(2000);
+        tempProgress.startAnimation(anim);
+    }
+
+    //this method the amount of sleep later
+    private  void getSleepTime () {
+        to = (float)80;
+        ProgressAnim anim = new ProgressAnim(sleepProgress, from, to);
+        anim.setDuration(2000);
+        sleepProgress.startAnimation(anim);
+    }
+
+    //this method the amount of steps later
+    private  void getStepNumber () {
+        to = (float)90;
+        ProgressAnim anim = new ProgressAnim(stepsProgress, from, to);
+        anim.setDuration(2000);
+        stepsProgress.startAnimation(anim);
     }
 
     public static TodayFragment newInstance() {
