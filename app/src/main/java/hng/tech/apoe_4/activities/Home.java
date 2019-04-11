f53778518e8f923966f64333656eb7410bf5ada9
Nnacheta Chimdike <yungdyke@gmail.com>
2019-04-11 11:51:44 +0100

    Update Home.java
    Added Pref code

--- app/src/main/java/hng/tech/apoe_4/activities/Home.java
+++ app/src/main/java/hng/tech/apoe_4/activities/Home.java
@@ -2,17 +2,14 @@ package hng.tech.apoe_4.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.pixplicity.easyprefs.library.Prefs;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.widget.Button;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
@@ -27,6 +24,8 @@ public class Home extends AppCompatActivity {
     @BindView(R.id.navigationView)
     BottomNavigationView bottomNavigationView;
 
+    @BindView(R.id.logout)
+    ImageView logoutImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +35,16 @@ public class Home extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 
 
+
         ButterKnife.bind(this);
 
+        openFragment(TodayFragment.newInstance());
 
+
+        logoutImageView.setOnClickListener(view -> {
+            Prefs.putString("accessToken", "");
+            startActivity(new Intent(Home.this, LoginActivity.class));
+        });
 
         BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
             switch (item.getItemId()){
@@ -58,7 +64,7 @@ public class Home extends AppCompatActivity {
                 }
 
                 default:{
-
+                    openFragment(ResultsFragment.newInstance());
                     return true;
                 }
             }
@@ -72,7 +78,7 @@ public class Home extends AppCompatActivity {
     private void openFragment(Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
+        transaction.addToBackStack(fragment.getTag());
         transaction.commit();
     }
 
