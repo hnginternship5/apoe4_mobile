d2a668523b7062d727292b4057cad227d8ec3b51
Akinyemi Mosolasi <41248079+Moshman20@users.noreply.github.com>
2019-04-13 16:33:19 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet,View, StatusBar,Dimensions,
+import {Platform, StyleSheet,View, StatusBar,Dimensions, DatePickerIOS,
 Image,Text,TextInput,TouchableOpacity} from 'react-native';
 
 var wS = Dimensions.get('window');
@@ -9,6 +9,16 @@ function hh(h){return (dh*h)/670}
 function ww(w){return (dw*w)/375}
 
 export default class App extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {chosenDate: new Date()};
+
+    this.setDate = this.setDate.bind(this);
+  }
+
+  setDate(newDate) {
+    this.setState({chosenDate: newDate});
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -16,42 +26,18 @@ export default class App extends React.Component {
           <View style={{height:'100%',justifyContent: 'center',alignItems:'center',width:'100%' }}>
               <View style={{height:'80%', width:'80%'}}>
               <Image source={require('./assets/logo.png')} style={{height:dw*0.2, width:dw*0.2, alignSelf:'center'}} resizeMode="contain"/> 
-                <Text style={{fontSize:ww(25), fontWeight:400}}>Sign Up</Text>
+                <Text style={{fontSize:ww(25), fontWeight:400}}>Date of Birth</Text>
 
                         <View style={{width:'100%', marginVertical:hh(50)}}>
-                          <TextInput 
-                          placeholder="Email Address"
-                          style={[styles.input]} />
-                          <TextInput 
-                          placeholder="Password"
-                          style={styles.input} />
-                          
-                        <View style={{flexDirection:'row',justifyContent:'space-between',marginBottom:hh(30),}}>
-                          <View style={{flexDirection:'row',alignItems:'center'}}>
-                          <View style={{height:15,width:15, borderRadius:30, borderWidth:1,borderColor:'blue',
-                          backgroundColor:'white',marginRight:10}}></View>
-                            <Text>Forgot Password?</Text>
-                          </View>
-
-                            <Text>Forgot Password</Text>
-                        </View>
+                                  <DatePickerIOS
+                                    date={this.state.chosenDate}
+                                    onDateChange={this.setDate}
+                                  />
                       </View>
-
                   <TouchableOpacity style={{width:'100%',paddingVertical:hh(20),marginBottom:hh(20), backgroundColor:'#3380CC',
                                   alignItems:'center', borderRadius:ww(5)}}>
-                    <Text style={{fontSize:ww(17),color:'white'}}
-                    >Sign in</Text>
-                  </TouchableOpacity>
-
-                <View style={{width:'100%', flexDirection:'row', justifyContent:'space-between'}}>
-                                  <TouchableOpacity style={styles.btn}>
-                      <Image source={require('./assets/fb.png')} style={{height:ww(30), width:ww(30)}} />
-                  </TouchableOpacity>
-
-                  <TouchableOpacity style={styles.btn}>
-                     <Image source={require('./assets/google.png')} style={{height:ww(30), width:ww(30)}} />
-                  </TouchableOpacity>
-                </View>
+                    <Text style={{fontSize:ww(19),color:'white'}}>Next</Text> 
+                    </TouchableOpacity>
 
               </View>
         </View>
@@ -72,18 +58,6 @@ const styles = StyleSheet.create({
     borderColor: '#3380CC',
     borderRadius: 5,
     borderWidth: 1.5
-  },
-  input:{
-    backgroundColor:'white',
-    height:hh(50),
-    width:'100%',
-    borderRadius:5,
-    borderWidth:2,
-    borderColor:'#3380CC',
-    marginBottom:hh(20)
-  },
-  mainTxt:{
-    fontSize:20,
   },
   container: {
     flex: 1,
