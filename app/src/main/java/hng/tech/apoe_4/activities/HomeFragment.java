9ce75587091358db0f670b73905d5e0b42013c7c
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-15 09:11:01 +0100

    Revert " Saved users WHG using shared preference for display on the Drawer Layout"

--- app/src/main/java/hng/tech/apoe_4/activities/HomeFragment.java
+++ app/src/main/java/hng/tech/apoe_4/activities/HomeFragment.java
@@ -1,11 +1,12 @@
 package hng.tech.apoe_4.activities;
 
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
 import hng.tech.apoe_4.R;
 
 /**
