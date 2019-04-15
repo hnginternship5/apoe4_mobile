5a42dd371211694a4f061d679c82527f05aa443b
Maaruf Dauda <mkdauda.work@gmail.com>
2019-04-15 20:30:29 +0100

    Begin Settings Activity

---
+++ app/src/main/java/hng/tech/apoe_4/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package hng.tech.apoe_4.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hng.tech.apoe_4.R;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
