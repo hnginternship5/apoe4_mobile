3377373ab0d9551ec41caa2d6323137970273af0
Nnacheta Chimdike <yungdyke@gmail.com>
2019-04-16 05:15:09 +0100

    Facebook Login

--- app/src/main/java/hng/tech/apoe_4/activities/LoginActivity.java
+++ app/src/main/java/hng/tech/apoe_4/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package hng.tech.apoe_4.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ import com.pixplicity.easyprefs.library.Prefs;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import hng.tech.apoe_4.ForgotPassword;
 import hng.tech.apoe_4.R;
 import hng.tech.apoe_4.RegisterActivity;
 import hng.tech.apoe_4.retrofit.responses.AuthResponse;
@@ -27,18 +29,45 @@ public class LoginActivity extends AppCompatActivity {
     private EditText login_email, login_password;
     RelativeLayout prog;
     TextView text5;
+    TextView forgotpass;
+    
+    LoginManager fbLoginManager;
+    List<String> permissionNeeds;
+    CallbackManager callbackManager;
+    ImageView fbButton;
+    String firstname;
 
     // todo: waiting on api to complete login task
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
+        
+        //set up callbacks and fbloginmanager
+        
+        callbackManager = CallbackManager.Factory.create();
+
+        permissionNeeds = Arrays.asList("email", "public_profile");
+        fbLoginManager = com.facebook.login.LoginManager.getInstance();
 
         login_email = findViewById(R.id.login_email);
         login_password = findViewById(R.id.login_password);
         text5 = findViewById(R.id.textView5);
         prog = findViewById(R.id.progress);
+        
+        //perform login
+        fbButton =findViewById(R.id.imageView4);
+
+        
+        fbButton.setOnClickListener(view ->{
+            fbLoginManager.logInWithReadPermissions(LoginActivity.this, permissionNeeds);
+            fblogin();
+                }
+
+        );
+        
     }
 
     public void login(View view) {
@@ -64,6 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                     prog.setVisibility(View.INVISIBLE);
                     text5.setVisibility(View.VISIBLE);
                     Toast.makeText(LoginActivity.this, "Login Success: " ,Toast.LENGTH_SHORT).show();
+                    Log.d("TAG", "onResponse: " + response.body().getAccessToken());
                     Prefs.putString("accessToken", response.body().getAccessToken());
                     if (!Prefs.getBoolean("savedDOB", false)) {
                         startActivity(new Intent(LoginActivity.this, DOB_page.class));
@@ -105,4 +135,135 @@ public class LoginActivity extends AppCompatActivity {
         }
         return false;
     }
+    public void passwordforgot(View view){
+        Intent intent = new Intent(this, ForgotPassword.class);
+        startActivity(intent);
+    }
+    
+    private void fblogin(){
+        //fblogin finally
+
+
+        fbLoginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+                    @Override
+                    protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                        if (currentAccessToken == null){
+                            return;
+                        }else {
+                            loaduserProfile(currentAccessToken);
+                        }
+                    }
+                };
+                Log.d("fb", "success");
+
+
+
+                String accessToken = loginResult.getAccessToken().getToken();
+                Log.d("accessToken", accessToken);
+                Prefs.putString("accessToken", accessToken);
+                Toast.makeText(LoginActivity.this, "Welcome " + firstname , Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(LoginActivity.this, DOB_page.class));
+
+
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Sign up cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(LoginActivity.this, "Oops an error occured", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+    private void registerFb(String fname, String lname, String regEmail, String regPassword){
+        MainApplication.getApiInterface().register(
+                fname,
+                lname,
+                regEmail,
+                regPassword)
+                .enqueue(new Callback<AuthResponse>() {
+                    @Override
+                    public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
+                        if (response.isSuccessful()){
+                            if (response.body() != null) {
+                                Toast.makeText(LoginActivity.this, "Welcome" + fname + " "+ lname,
+                                        Toast.LENGTH_SHORT).show();
+
+
+                                //startActivity(new Intent(LoginActivity.this, DOB_page.class));
+                            }
+                        }
+//                        Toast.makeText(RegisterActivity.this,"",
+//                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<AuthResponse> call, Throwable t) {
+
+                        //Todo Logic to handle failure
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //USe graph Api to get Facebook details
+
+    private void loaduserProfile(AccessToken token){
+        GraphRequest request  = GraphRequest.newMeRequest(token, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    firstname  = object.getString("first_name");
+                    String lname = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                    String image_url = "http://graph.facebook.com/" + id + "/picture?type=large";
+
+                    Prefs.putString("fullname", firstname + " " + lname);
+                    Prefs.putString("url", image_url);
+                    Log.d("fb", image_url);
+                    Log.d("fb", firstname);
+                    Prefs.putString("email", email);
+                    Prefs.putBoolean("regFb", true); 
+
+                    registerFb(firstname, lname, email, "APEO4" + token.getToken());
+
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name, last_name, email, id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
+    
 }
