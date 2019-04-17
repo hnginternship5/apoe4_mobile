d71edc5d3e9fd15feb9408e17c64b536f6a9b729
Moshman20 <akinyemimosolasi@gmail.com>
2019-04-17 11:48:49 +0100

    set for expo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet,View, StatusBar,Dimensions, DatePickerIOS,
+import {Platform, StyleSheet,View, StatusBar,Dimensions,
 Image,Text,TextInput,TouchableOpacity} from 'react-native';
 
 var wS = Dimensions.get('window');
@@ -9,16 +9,6 @@ function hh(h){return (dh*h)/670}
 function ww(w){return (dw*w)/375}
 
 export default class App extends React.Component {
-    constructor(props) {
-    super(props);
-    this.state = {chosenDate: new Date()};
-
-    this.setDate = this.setDate.bind(this);
-  }
-
-  setDate(newDate) {
-    this.setState({chosenDate: newDate});
-  }
   render() {
     return (
       <View style={styles.container}>
@@ -26,18 +16,33 @@ export default class App extends React.Component {
           <View style={{height:'100%',justifyContent: 'center',alignItems:'center',width:'100%' }}>
               <View style={{height:'80%', width:'80%'}}>
               <Image source={require('./assets/logo.png')} style={{height:dw*0.2, width:dw*0.2, alignSelf:'center'}} resizeMode="contain"/> 
-                <Text style={{fontSize:ww(25), fontWeight:400}}>Date of Birth</Text>
+                <Text style={{fontSize:ww(25), fontWeight:400}}>Sign Up</Text>
 
                         <View style={{width:'100%', marginVertical:hh(50)}}>
-                                  <DatePickerIOS
-                                    date={this.state.chosenDate}
-                                    onDateChange={this.setDate}
-                                  />
+                       <TextInput 
+                          placeholder="Full name"
+                          style={styles.input} />
+                         <TextInput 
+                          placeholder="Email Address"
+                          style={styles.input} />
+                          <TextInput 
+                          placeholder="Password"
+                          style={[styles.input]} />
+                          <TextInput 
+                          placeholder="Confirm Password"
+                          style={styles.input} />
+                    <View style={{flexDirection:'row',alignItems:'center', width:'100%'}}>
+                          <View style={{height:15,width:15, borderRadius:30, borderWidth:1,borderColor:'blue',
+                          backgroundColor:'white',marginRight:10}}></View>
+                            <Text>Accept Terms and Conditions?</Text>
+                          </View>
                       </View>
+
                   <TouchableOpacity style={{width:'100%',paddingVertical:hh(20),marginBottom:hh(20), backgroundColor:'#3380CC',
                                   alignItems:'center', borderRadius:ww(5)}}>
-                    <Text style={{fontSize:ww(19),color:'white'}}>Next</Text> 
-                    </TouchableOpacity>
+                    <Text style={{fontSize:ww(17),color:'white'}}
+                    >Sign Up</Text>
+                  </TouchableOpacity>
 
               </View>
         </View>
@@ -58,6 +63,18 @@ const styles = StyleSheet.create({
     borderColor: '#3380CC',
     borderRadius: 5,
     borderWidth: 1.5
+  },
+  input:{
+    backgroundColor:'white',
+    height:hh(50),
+    width:'100%',
+    borderRadius:5,
+    borderWidth:2,
+    borderColor:'#3380CC',
+    marginBottom:hh(20)
+  },
+  mainTxt:{
+    fontSize:20,
   },
   container: {
     flex: 1,
