a6b3f86df76a7afa4edd07608a51f28f2b78f35e
lovisgod <olifedayo94@gmail.com>
2019-04-18 16:41:23 +0100

    FT-implemented result page layout

--- app/src/main/java/hng/tech/apoe_4/fragments/ResultsFragment.java
+++ app/src/main/java/hng/tech/apoe_4/fragments/ResultsFragment.java
@@ -1,7 +1,9 @@
 package hng.tech.apoe_4.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -9,14 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.fragment.app.FragmentTransaction;
 import hng.tech.apoe_4.R;
@@ -24,7 +29,7 @@ import hng.tech.apoe_4.R;
 
 public class ResultsFragment extends Fragment {
 
-    private BarChart chart;
+
 
     public ResultsFragment(){
 
@@ -36,46 +41,23 @@ public class ResultsFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_results, container, false);
 
         //here we set the datas for the bar chart
-         chart = v.findViewById(R.id.chart);
-        ArrayList score = new ArrayList();
 
-        score.add(new BarEntry(10f, 0));
-        score.add(new BarEntry(4f, 1));
-        score.add(new BarEntry(6f, 2));
-        score.add(new BarEntry(8f, 3));
-        score.add(new BarEntry(10f, 4));
-        score.add(new BarEntry(2f, 5));
-        score.add(new BarEntry(10f, 6));
+        BarChart barChart = v.findViewById(R.id.chart);
 
+        // the labels that should be drawn on the XAxis
+        final String[] quarters = new String[] { "SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT" };
+        IAxisValueFormatter formatter = (value, axis) -> quarters[(int) value];
 
-        ArrayList dayAxis = new ArrayList();
-
-        dayAxis.add("SUN");
-        dayAxis.add("MON");
-        dayAxis.add("TUE");
-        dayAxis.add("WED");
-        dayAxis.add("THUR");
-        dayAxis.add("FRI");
-        dayAxis.add("SAT");
-
-
-        BarDataSet bardataset = new BarDataSet(score, "Score");
-        chart.animateY(5000);
-        BarData data = new BarData(dayAxis, bardataset);
-        bardataset.setColors(R.color.colorAccent);
-        chart.setData(data);
-
-
-
-
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+        BarData barData = new BarData(getDataSet());
+        barChart.setData(barData);
+        barChart.animateXY(2000, 2000);
+        barChart.invalidate();
 
         Button button=v.findViewById(R.id.backbutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(TodayFragment.newInstance(), "result");
-            }
-        });
+        button.setOnClickListener(v1 -> openFragment(TodayFragment.newInstance(), "result"));
         return v;
     }
     private void openFragment(Fragment fragment, String tag){
@@ -86,6 +68,34 @@ public class ResultsFragment extends Fragment {
         transaction.commit();
     }
 
+    private ArrayList<IBarDataSet> getDataSet() {
+        ArrayList<IBarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(0, 10f); // sun
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(1, 10f); // mon
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(2, 4f); // tue
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(3, 4f); // wed
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(4, 2f); // thur
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(5, 8f); // fri
+        valueSet1.add(v1e6);
+        BarEntry v1e7 = new BarEntry(6, 10f); // sat
+        valueSet1.add(v1e7);
+
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "APO SCORE");
+        barDataSet1.setColor(ContextCompat.getColor(getContext(),R.color.figmaBlue));
+
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet1);
+        return dataSets;
+    }
 
 
 
