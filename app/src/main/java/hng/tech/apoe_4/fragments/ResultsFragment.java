37fad23642ef5edc3953cf85bf95fc9806a0a0df
lovisgod <olifedayo94@gmail.com>
2019-04-18 09:57:18 +0100

    FT-started graph view

--- app/src/main/java/hng/tech/apoe_4/fragments/ResultsFragment.java
+++ app/src/main/java/hng/tech/apoe_4/fragments/ResultsFragment.java
@@ -10,11 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+
 import androidx.fragment.app.FragmentTransaction;
 import hng.tech.apoe_4.R;
 
 
 public class ResultsFragment extends Fragment {
+
+    private BarChart chart;
 
     public ResultsFragment(){
 
@@ -24,6 +34,40 @@ public class ResultsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_results, container, false);
+
+        //here we set the datas for the bar chart
+         chart = v.findViewById(R.id.chart);
+        ArrayList score = new ArrayList();
+
+        score.add(new BarEntry(10f, 0));
+        score.add(new BarEntry(4f, 1));
+        score.add(new BarEntry(6f, 2));
+        score.add(new BarEntry(8f, 3));
+        score.add(new BarEntry(10f, 4));
+        score.add(new BarEntry(2f, 5));
+        score.add(new BarEntry(10f, 6));
+
+
+        ArrayList dayAxis = new ArrayList();
+
+        dayAxis.add("SUN");
+        dayAxis.add("MON");
+        dayAxis.add("TUE");
+        dayAxis.add("WED");
+        dayAxis.add("THUR");
+        dayAxis.add("FRI");
+        dayAxis.add("SAT");
+
+
+        BarDataSet bardataset = new BarDataSet(score, "Score");
+        chart.animateY(5000);
+        BarData data = new BarData(dayAxis, bardataset);
+        bardataset.setColors(R.color.colorAccent);
+        chart.setData(data);
+
+
+
+
 
         Button button=v.findViewById(R.id.backbutton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +85,9 @@ public class ResultsFragment extends Fragment {
         Log.d("TAG","fragment tag: "+fragment.getTag());
         transaction.commit();
     }
+
+
+
 
     public static ResultsFragment newInstance(){
         return new ResultsFragment();
