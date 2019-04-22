2ff21df383280ecdeafd51e3322d4ee10c21fd80
thejsdev <uchenoeldike@gmail.com>
2019-04-22 00:27:51 -0700

    added app loading screen

--- apoe/App.js
+++ apoe/App.js
@@ -1,21 +1,35 @@
-import React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+// Use this screen to load any assets
 
+// this is the app navigotor component set in the navigation folder... do not delete this!
 import AppNavigator from "./navigation/AppNavigator";
+import React from "react";
+import { Asset, AppLoading } from "expo";
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false
+    isReady: false
   };
 
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return <AppNavigator />;
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
+  async _cacheResourcesAsync() {
+    const images = [require("./assets/fb.png"), require("./assets/logo.png")];
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
   }
-});
+}
