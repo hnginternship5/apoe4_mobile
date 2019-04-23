6c8094b13d02c6ff291c39b4d78083164ef397d7
thejsdev <uchenoeldike@gmail.com>
2019-04-23 12:00:22 -0700

    created custom text component

--- apoe/components/StyledText.js
+++ apoe/components/StyledText.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
-export class MonoText extends React.Component {
+export class Txt extends React.Component {
   render() {
-    return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />;
+    return (
+      <Text
+        {...this.props}
+        style={[
+          this.props.style,
+          { fontFamily: "quick-light", color: "white" }
+        ]}
+      />
+    );
+  }
+}
+
+export class TxtB extends React.Component {
+  render() {
+    return (
+      <Text
+        {...this.props}
+        style={[
+          this.props.style,
+          { fontFamily: "quick-regular", color: "white", fontSize: 20 }
+        ]}
+      />
+    );
   }
 }
